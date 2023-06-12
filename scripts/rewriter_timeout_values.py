540af0a9fc6b0d0fac8b3e1d6a38bd806b004104
Bogdan-Alexandru Stoica <b.al.stoica@gmail.com>
2023-06-12 01:30:13 -0500

    Change script to overwrite the existing file, but also make a copy of the original

--- scripts/rewriter_timeout_values.py
+++ scripts/rewriter_timeout_values.py
@@ -1,8 +1,12 @@
+
+import argparse
 import os
 import re
-import argparse
+import shutil
+
 
 TIMEOUT_THRESHOLD = 300000
+
 
 def read_test_targets(test_targets_file):
     """
@@ -19,131 +23,103 @@ def read_test_targets(test_targets_file):
     return test_targets
 
 
-def modify_timeout_annotations(test_files_dir, test_targets):
+def modify_timeout_annotations(file_path):
     """
-    Modify test timeout values for a the given list of test targets.
+    Modify test timeout values for the given list of Java test targets.
 
     Args:
-        test_files_dir (str): Directory path containing the Java test files.
-        test_targets (list): List of test targets to be modified.
+        file_path (str): The Java test file to be modified.
     """
-    for root, _, files in os.walk(test_files_dir):
-        for file_name in files:
-            if file_name.endswith(".java") and file_name.startswith("Test"):
-                file_path = os.path.join(root, file_name)
 
-                with open(file_path, "r") as test_file:
-                    file_lines = test_file.readlines()
+    with open(file_path, "r") as test_file:
+        file_lines = test_file.readlines()
 
-                modified_lines = []
-                for line in file_lines:
-                    modified_line = line
-
-                    # Remove spaces within the line
-                    line_without_spaces = re.sub(r"\s", "", line)
-
-                    if any(test_target in line_without_spaces for test_target in test_targets):
-                        if "@Test" in line_without_spaces and "timeout" in line_without_spaces:
-                            if re.search(r"@Test\(timeout=(\d+)\)", line_without_spaces):
-                                current_timeout = int(match.group(1))
-                                if current_timeout < TIMEOUT_THRESHOLD:
-                                    modified_timeout = str(TIMEOUT_THRESHOLD)
-                                    modified_line = re.sub(
-                                        r"@Test\(timeout=\d+\)",
-                                        r"@Test (timeout = {0})".format(modified_timeout),
-                                        line,
-                                    )
-
-                    modified_lines.append(modified_line)
-
-                modified_lines = modify_wait_for_calls(modified_lines)
-
-                write_modified_files(file_path, modified_lines)
-
-
-def modify_wait_for_calls(lines):
-    """
-    modify the timeout values in GenericTestUtils.waitFor calls.
-
-    Args:
-        lines (list): List of lines in the Java test file.
-
-    Returns:
-        list: Modified list of lines.
-    """
     modified_lines = []
-    pattern = r"GenericTestUtils\.waitFor\([^)]+\)"
-
-    for line in lines:
+    for line in file_lines:
         modified_line = line
-        if re.search(pattern, line):
-            matches = re.findall(pattern, line)
-            for match in matches:
-                modified_wait_for_call = modify_wait_for_timeout(match)
-                modified_line = modified_line.replace(
-                    match, 
-                    modified_wait_for_call, 1)
+
+        # Remove spaces within the line
+        line_without_spaces = re.sub(r"\s", "", line)
+
+        # Check if the "timeout" annotation is present, and modify it
+        if "@Test" in line_without_spaces and "timeout" in line_without_spaces:
+            if re.search(r"@Test\(timeout=(\d+)\)", line_without_spaces):
+                current_timeout = int(re.search(r"@Test\(timeout=(\d+)\)", line_without_spaces).group(1))
+                match = current_timeout < TIMEOUT_THRESHOLD
+                if match:
+                    modified_timeout = str(TIMEOUT_THRESHOLD)
+                    modified_line = re.sub(
+                        r"@Test\(timeout=\d+\)",
+                        r"@Test (timeout = {0})".format(modified_timeout),
+                        line_without_spaces,
+                    )
 
         modified_lines.append(modified_line)
 
-    return modified_lines
+    with open(file_path, "w") as test_file:
+        test_file.writelines(modified_lines)
 
 
-def modify_wait_for_timeout(wait_for_call):
+def modify_wait_for_calls(file_path):
     """
-    Modify the timeout value in the GenericTestUtils.waitFor call.
+    Modify the timeout values in GenericTestUtils.waitFor calls inside Java test files.
 
     Args:
-        wait_for_call (str): The GenericTestUtils.waitFor call.
-
-    Returns:
-        str: The modified GenericTestUtils.waitFor call.
+        file_path (str): The Java test file to be modified.
     """
-    timeout_values = re.findall(r"\d+", wait_for_call)
-    last_timeout = timeout_values[-1]
-    if int(last_timeout) < 300000:
-        timeout_values[-1] = "300000"
-    modified_wait_for_call = wait_for_call.replace(
-        last_timeout, 
-        timeout_values[-1])
-    return modified_wait_for_call
+    modified_lines = []
 
+    with open(file_path, "r") as test_file:
+        for line in test_file:
+            modified_line = line
 
-def write_modified_files(file_path, modified_lines):
-    """
-    Write the modified lines to a new Java test file.
+            if "GenericTestUtils.waitFor" in line:
+                match = re.search(r"GenericTestUtils.waitFor.*?\((.*?),(.*?),(.*?)\);", line):
+                if match:
+                    last_value = match.group(3)
+                    if last_value.isnumeric() and int(last_value) < TIMEOUT_THRESHOLD:
+                        modified_line = line.replace(last_value, str(TIMEOUT_THRESHOLD))
 
-    Args:
-        file_path (str): Path to the original Java test file.
-        modified_lines (list): List of modified lines.
+            modified_lines.append(modified_line)
 
-    Returns:
-        str: Path to the new Java test file.
-    """
-    new_file_path = file_path.replace(".java", "_modified.java")
-    with open(new_file_path, "w") as new_file:
-        new_file.writelines(modified_lines)
-
-    return new_file_path
+    with open(file_path, "w") as test_file:
+        test_file.writelines(modified_lines)
 
 
 def main():
+    # Parse command-line arguments
     parser = argparse.ArgumentParser(
-        description="modify Java test files to update timeout values."
+        description="Modify test files to update timeout values."
         )
     parser.add_argument(
         "test_names_file", 
-        help="Path to the file containing the test targets."
+        help="Path to the file containing the target test files."
         )
     parser.add_argument(
         "test_files_dir", 
-        help="Directory path containing the Java test files."
+        help="Directory path containing the target test files."
         )
     args = parser.parse_args()
 
+    # Read the test targets from the file
     test_targets = read_test_targets(args.test_names_file)
-    modify_timeout_annotations(args.test_files_dir, test_targets)
+    
+    # Traverse through the directory and process the test files
+    for root, _, files in os.walk(args.test_files_dir):
+        for file_name in files:
+            # Process only Java test files starting with "Test"
+            if file_name.endswith(".java") and file_name.startswith("Test"):
+                file_path = os.path.join(root, file_name)
+                
+                # Create a copy of the original file
+                original_file_path = f"{file_path}.original"
+                shutil.copy2(file_path, original_file_path)
+
+                # Modify the timeout annotations and wait_for statements
+                modify_timeout_annotations(file_path)
+                modify_wait_for_calls(file_path)
 
 
 if __name__ == "__main__":
     main()
+
