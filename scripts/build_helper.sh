619a1c99070e64a1c3b190bf4a6395f790c7264d
Bogdan Alexandru Stoica <bastoica@uchicago.edu>
2023-06-18 16:20:03 -0500

    Fix past logs overwritting bug

--- scripts/build_helper.sh
+++ scripts/build_helper.sh
@@ -25,9 +25,8 @@ fi
 
 
 # Compile, build, and test the target application
-mvn clean 2>&1 | tee -a ${log_file} && \
-    mvn -fn -DskipTests -DcsvFileName="${config_file}" compile 2>&1 | tee -a ${log_file} && \
-        mvn -DcsvFileName="${config_file}" -DmaxInjections="${max_injections}" -Dparallel-tests -DtestsThreadCount=${threads} -fn test 2>&1 | tee -a ${log_file}
+mvn -fn -DskipTests -DcsvFileName="${config_file}" clean compile 2>&1 | tee -a ${log_file} && \
+    mvn -DcsvFileName="${config_file}" -DmaxInjections="${max_injections}" -Dparallel-tests -DtestsThreadCount=${threads} -fn test 2>&1 | tee -a ${log_file}
 
 
 # Make the log file UTF-8 compliant
@@ -41,7 +40,7 @@ date=$(date -d "today" +"%Y%m%d%H%M")
 mkdir -p ${wasabi_dir}/${date}/test_reports
 mv ${log_file} ${wasabi_dir}/${date}
 
-for file in $(find . -name "*-output.txt"); do
+for file in $(find . -name "*-output.txt" | grep -ve "wasabi.data"); do # WARNING: 'grep' critical to not overwrite past logs
     fname=`basename $file`
     mv ${file} ${wasabi_dir}/${date}/test_reports/${fname}
 done
