2a77cecee6b33b38def18a2f1aca7060b37f93ba
Bogdan Alexandru Stoica <bastoica@uchicago.edu>
2023-06-18 14:22:25 -0500

    Refactor and improve the coverage data collection script

--- scripts/retry_coverage.py
+++ scripts/retry_coverage.py
@@ -1,84 +1,106 @@
-#! /usr/bin/python3
-
 import argparse
 import os
-import sys
+import re
+from collections import Counter
 
-_INJECTION_LOCATIONS = [
-    "WebHdfsFileSystem.java:839", 
-    "ObserverReadProxyProvider.java:455", 
-    "Balancer.java:822", 
-    "ReencryptionUpdater.java:441",
-    "TimelineConnector.java:352",
-    "DelegationTokenRenewer.java:1014",
-    "FederationActionRetry.java:35",
-    "FileSystemTimelineWriterImpl.java:278",
-    "LogsCLI.java:1560",
-    "UserGroupInformation.java:995",
-    "Client.java:678",
-    "RPC.java:431",
-    "EditLogTailer.java:633",
-    "SecondaryNameNode.java:368",
-    "ProvidedVolumeImpl.java:173",
-    "DataXceiverServer.java:235",
-    "BPServiceActor.java:825",
-    "ExternalSPSBlockMoveTaskHandler.java:218",
-    "JobEndNotifier.java:88",
-    "Task.java:1414",
-    "ClientServiceDelegate.java:335",
-    "DFSInputStream.java:360",
-    "DFSInputStream.java:662",
-    "DFSInputStream.java:808",
-    "DFSInputStream.java:880",
-    "DFSInputStream.java:1214",
-    "ShortCircuitCache.java:214",
-    "ShortCircuitCache.java:731",
-    "LoadBalancingKMSClientProvider.java:184",
-    "RouterRpcClient.java:690",
-    "UserGourpInformation.java:996",
-    "WebAppUtil.java:114",
-]
 
-CoverageMap = {}
+def count_retry_locations(filename):
+    """
+    Counts the number of substrings flanked by "++" in a given file.
 
-def parseTestLog(fname: str):
-    with open(fname, "r") as fin:
-        lines = fin.readlines()
+    Args:
+        filename (str): Path to the file.
 
-    maxRetriesMap = {}
-    index = 0
-    while index < len(lines):
-        for iloc in _INJECTION_LOCATIONS:
-            if index < len(lines) and "[wasabi]:" in lines[index] and iloc in lines[index] and "thrown, fault count: " in lines[index]:
-                counter = int(lines[index].split("thrown, fault count: ")[1].split(",")[0])
-                if counter == 1:
-                    CoverageMap[iloc] = CoverageMap.get(iloc, 0) + 1
-                if maxRetriesMap.get(iloc, 0) < counter:
-                    maxRetriesMap[iloc] = counter
-        index += 1
+    Returns:
+        dict: Dictionary containing the counts of substrings.
+    """
+    pattern = r"~~(.*?)~~"
+    counts = Counter()
 
-    if len(maxRetriesMap) > 0:
-        print("\n------ " + os.fsdecode(fname) + " ------")
-        for key in maxRetriesMap.keys():
-            print(str(key).ljust(50) + ": " + str(maxRetriesMap[key]))
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            matches = re.findall(pattern, line)
+            counts.update(matches)
+
+    return counts
+
+
+def update_retries_per_files_counts(retry_locations_to_tests, retries_per_file):
+    """
+    Updates the pattern files count based on the counts from a file.
+
+    Args:
+        retry_locations_to_tests (dict): Dictionary to store the pattern file counts.
+        retries_per_file (dict): Dictionary containing the counts from a file.
+    """
+    for rloc in retries_per_file:
+        if rloc in retry_locations_to_tests:
+            retry_locations_to_tests[rloc] += 1
+        else:
+            retry_locations_to_tests[rloc] = 1
+
+
+def directory_walk(path):
+    """
+    Recursively walks through the directory and processes each file.
+
+    Args:
+        path (str): Directory path to start the walk.
+
+    Returns:
+        Tuple: Aggregated counts and pattern file counts.
+    """
+    retry_locations = Counter()
+    retry_locations_to_tests = Counter()
+
+    for root, _, files in os.walk(path):
+        for file in files:
+            if file.endswith("-output.txt"):
+                file_path = os.path.join(root, file)
+                retries_per_file = count_retry_locations(file_path)
+                retry_locations.update(retries_per_file)
+                update_retries_per_files_counts(retry_locations_to_tests, retries_per_file)
+
+    return retry_locations, retry_locations_to_tests
+
+
+def print_counts(retry_locations, retry_locations_to_tests=None):
+    """
+    Prints the counts in descending order.
+
+    Args:
+        retry_locations (Counter): Dictionary of retry location counts.
+        retry_locations_to_tests (dict, optional): Dictionary of retry locations to test counts.
+            Defaults to None.
+        directory_name (str, optional): Directory name. Defaults to None.
+    """
+    print("==== Retry location : aggregted number of times exercised by a test ====\n")
+    for rloc, count in retry_locations.most_common():
+        print(f"{rloc} : {count}")
+
+    if retry_locations_to_tests is not None:
+        print("\n\n==== Retry location : number of tests covering it ====\n")
+        for rloc, count in retry_locations_to_tests.items():
+            print(f"{rloc}: {count}")
+   
+    print("\n\n")
+
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-       "-p", "--path",
-       help="Prune mirror exceptions errors"
-    )
+    parser = argparse.ArgumentParser(description='Substring Counter')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-f', dest='filename', help='Path to a single file')
+    group.add_argument('-p', dest='directory', help='Path to a directory')
+
     args = parser.parse_args()
-    
-    if args.path:
-        for f in os.listdir(args.path):
-            fname = os.fsdecode(f)
-            if fname.endswith("-output.txt"):
-                parseTestLog(os.path.join(args.path, f))
 
-    print("\n\n\n============== C O V E R A G E  S T A T S ==============")
-    for key in CoverageMap.keys():
-        print(str(key).ljust(50) + ": " + str(CoverageMap[key]))
+    if args.filename:
+        counts = count_retry_locations(args.filename)
+        print_counts(counts)
 
-if __name__ == "__main__":
+    elif args.directory:
+        counts, counts_per_file = directory_walk(args.directory)
+        print_counts(counts, counts_per_file, directory_name=args.directory)
+
+if __name__ == '__main__':
     main()
