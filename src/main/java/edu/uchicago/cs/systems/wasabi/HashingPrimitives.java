5b4177462413fecde0ca94b59701a36dfda4b321
Bogdan-Alexandru Stoica <b.al.stoica@gmail.com>
2023-07-26 02:48:49 -0500

    Re-write the hashing package to use MurmurHash3

--- src/main/java/edu/uchicago/cs/systems/wasabi/HashingPrimitives.java
+++ src/main/java/edu/uchicago/cs/systems/wasabi/HashingPrimitives.java
@@ -1,24 +1,40 @@
 package edu.uchicago.cs.systems.wasabi;
 
+import java.nio.charset.StandardCharsets;
+import org.apache.commons.codec.digest.MurmurHash3;
 import java.util.ArrayList;
-import java.util.StringJoiner;
 
 class HashingPrimitives {
   public static int getHashValue(String str1, String str2, String str3) {
-    StringJoiner joiner = new StringJoiner("@");
-    joiner.add(str1);
-    joiner.add(str2);
-    joiner.add(str3);
-
-    return joiner.toString().hashCode();
+    byte[] bytes1 = str1.getBytes(StandardCharsets.UTF_8);
+    byte[] bytes2 = str2.getBytes(StandardCharsets.UTF_8);
+    byte[] bytes3 = str3.getBytes(StandardCharsets.UTF_8);
+    
+    byte[] bytes = new byte[bytes1.length + bytes2.length + bytes3.length];
+    
+    System.arraycopy(bytes1, 0, bytes, 0, bytes1.length);
+    System.arraycopy(bytes2, 0, bytes, bytes1.length, bytes2.length);
+    System.arraycopy(bytes3, 0, bytes, bytes1.length + bytes2.length, bytes3.length);
+    
+    return MurmurHash3.hash32x86(bytes, 0, bytes.length, 0);
   }
 
   public static int getHashValue(ArrayList<String> arr) {
-    StringJoiner joiner = new StringJoiner("@");
+    ArrayList<byte[]> byteList = new ArrayList<>();
+    int totalLength = 0;
     for (String e : arr) {
-      joiner.add(e);
+      byte[] bytes = e.getBytes(StandardCharsets.UTF_8);
+      byteList.add(bytes);
+      totalLength += bytes.length;
     }
-
-    return joiner.toString().hashCode();
+    
+    byte[] bytes = new byte[totalLength];
+    int offset = 0;
+    for (byte[] b : byteList) {
+      System.arraycopy(b, 0, bytes, offset, b.length);
+      offset += b.length;
+    }
+    
+    return MurmurHash3.hash32x86(bytes, 0, bytes.length, 0);
   }
 }
