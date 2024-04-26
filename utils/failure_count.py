b10dccb359f814d67f883cc610dce5d3f8a2d596
Bogdan-Alexandru Stoica <b.al.stoica@gmail.com>
2024-04-26 13:35:25 -0500

    Core dump after the SOSP'24 submission

--- utils/failure_count.py
+++ utils/failure_count.py
@@ -3,15 +3,15 @@ import sys
 import re
 
 def parse_logs(directory_path):
-  results = {}
+  test_executed = set()
   for dirpath, dirnames, filenames in os.walk(directory_path):
     for filename in filenames:
       if filename.endswith("-output.txt"):
         file_path = os.path.join(dirpath, filename)
         with open(file_path, 'r') as file:
           for line in file:
-            if "[wasabi]" in line and "[Pointcut]" in line:
-              line = line[line.index("[Pointcut]"):]
+            if "[wasabi]" in line and "[FAILURE]" in line:
+              line = line[line.index("[FAILURE]"):]
               
               parts = line.split("|")
               test_name = ""
@@ -22,21 +22,10 @@ def parse_logs(directory_path):
                   if match:
                     # Adjusted to remove the trailing period
                     test_name = match.group(0)[:-2]
-                elif "Retry caller ---" in part:
-                  retry_caller_match = re.search(r"---(.+?)---", part)
-                  if retry_caller_match:
-                    retry_caller = retry_caller_match.group(1)
-              if test_name and retry_caller:
-                if retry_caller not in results:
-                  results[retry_caller] = [test_name]
-                elif test_name not in results[retry_caller]:
-                  results[retry_caller].append(test_name)
+                    test_executed.add(test_name)
 
-    # for retry_caller in results.keys():
-    #   print(retry_caller)
-    for test_names in results.values():
-      for test_name in test_names:
-          print(test_name)
+    for test_name in test_executed:
+      print(test_name)
 
 if __name__ == "__main__":
   if len(sys.argv) < 2:
